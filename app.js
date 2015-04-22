1736ee64f74ca9a5589899779aa73c51bddbb51f
vincnetas <vincnetas@gmail.com>
2015-04-22 15:47:16 +0300

    Initial socket io

---
+++ app.js
@@ -0,0 +1,21 @@
+var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var port = process.env.OPENSHIFT_NODEJS_PORT || '3000';
+
+var path = require('path');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(port, host);
+
+app.use(express.static(path.normalize(__dirname) + '/public'));
+
+io.on('connection', function (socket) {
+    socket.emit('news', {
+        hello: 'world'
+    });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
